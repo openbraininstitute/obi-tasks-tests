28dc53fda04df9f19a567cb9144fa26673a291b4
Gianluca Ficarelli <gianluca.ficarelli@openbraininstitute.org>
2026-07-05 11:52:30 +0200

    Debug path

--- tasks/validation/code.py
+++ tasks/validation/code.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from functools import partial
+from pathlib import Path
 
 from entitysdk import Client, models
 from entitysdk.token_manager import TokenFromFunction
@@ -8,12 +9,30 @@ from obi_auth import get_token
 
 
 def validate_morphology(client, entity_id) -> dict:
-    morphology = client.get_entity(entity_type=models.CellMorphology, entity_id=entity_id)
+    morphology = client.get_entity(
+        entity_type=models.CellMorphology, entity_id=entity_id
+    )
     assert morphology.type == "cell_morphology"
     return {"entity_id": str(morphology.id)}
 
 
+def ls(path):
+    p = Path(path)
+    if not p.exists():
+        print(f"{path} does not exist")
+        return
+    for directory in sorted(p.rglob("*")):
+        if directory.is_dir():
+            print(f"{directory}:")
+            for f in sorted(directory.iterdir()):
+                if f.is_file():
+                    s = f.stat()
+                    print(f"  {oct(s.st_mode)[-3:]}  {s.st_size:>10}  {f.name}")
+
+
 if __name__ == "__main__":
+    ls("/data/aws_s3_internal/private")
+
     entity_id = sys.argv[1]
 
     deployment = os.environ["DEPLOYMENT"]
